199d1970a222e02f140ffb792ac6e472a3635473
Oluwademilade <demiladeomisore@outlook.com>
2022-10-13 16:53:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /** 
- * Main - Entry
- * Return - 0
- */
+*Function
+* Main-Entry
+* Return-Always 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
