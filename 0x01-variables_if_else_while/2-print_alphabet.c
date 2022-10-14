e398dc05954cd60dc402a541e77f15ba35fb642a
Oluwademilade <demiladeomisore@outlook.com>
2022-10-14 16:20:59 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lower case letters
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	putchar ('\n');
+	}
+	return (0);
+}
