1ace3895c8ef9c391e3729867e2eb9f2a043385b
Oluwademilade <demiladeomisore@outlook.com>
2022-10-14 04:03:00 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	printf("Size of char: %i bytes\n", sizeof(char));
+	printf("Size of int: %i bytes\n", sizeof(int));
+	printf("Size of long int: %i bytes\n", sizeof(long int));
+	printf("Size of long long int: %i bytes\n", sizeof(long long int));
+	printf("Size of float: %i bytes\n", sizeof(float));
+	return (0);
