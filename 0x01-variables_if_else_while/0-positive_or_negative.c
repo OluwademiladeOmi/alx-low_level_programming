dbca1d1cbcffc966146647e9d777d5bfa1ad23e2
Oluwademilade <demiladeomisore@outlook.com>
2022-10-14 12:49:07 -0700

    postive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	int n;
+
+	printf("Please enter your number")
+		scanf("%d", n);
+	if (n > 0)
+	{
+		printf("The number is postive!\n");
+	}
+	else if (n == 0)
+	{
+		printf("The number is zero\n");
+	}
+	else if (n < 0)
+	{
+		printf("The number is negative\n");
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
