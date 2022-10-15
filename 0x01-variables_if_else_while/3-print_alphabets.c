39bdd68815554bd85791dab279eb42997474a890
Oluwademilade <demiladeomisore@outlook.com>
2022-10-14 17:58:53 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints in lower and uppercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= "Z"; ch++)
+	{
+		putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
+
+
