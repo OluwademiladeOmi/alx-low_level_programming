e976c7322096146291eb19c0d388bddab7e10855
Oluwademilade <demiladeomisore@outlook.com>
2022-10-17 14:43:53 -0700

    believe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main-program entry point.
+ *
+ *Return: 0 no error, non zero if error
+ */
+
+int main(void)
+{
+		int d;
+
+		for (d = '0'; d <= '9'; d++)
+			putchar(d);
+			putchar('\n');
+				return (0);
+}
