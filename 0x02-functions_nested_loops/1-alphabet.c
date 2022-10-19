16983958db17e0edfb03aa1de2b3f8567cf23555
Oluwademilade <demiladeomisore@outlook.com>
2022-10-19 14:47:49 -0700

    jlo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - Enrty point
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
